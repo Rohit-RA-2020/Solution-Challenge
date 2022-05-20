a473be2f338b2a6dbfecf8664fc6b973111b6b41
Rohit Ranjan <ranjanrohit812@gmail.com>
2022-05-21 00:55:51 +0530

    Splash Screen Added

--- reduce_footprint/lib/main.dart
+++ reduce_footprint/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:reduce_footprint/Screens/onBoarding/onboarding_screen.dart';
 import 'package:reduce_footprint/providers/user_provider.dart';
 import 'package:reduce_footprint/responsive/responsive_layout.dart';
@@ -15,7 +16,113 @@ void main() async {
 
   await Firebase.initializeApp();
 
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      title: 'Reduce Footprint',
+      home: IntroPage(),
+    ),
+  );
+}
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
+  late AnimationController _lottieAnimation;
+  var expanded = false;
+  final double _bigFontSize = 178;
+  final transitionDuration = const Duration(seconds: 1);
+
+  @override
+  void initState() {
+    _lottieAnimation = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    Future.delayed(const Duration(seconds: 1))
+        .then((value) => setState(() => expanded = true))
+        .then((value) => const Duration(seconds: 1))
+        .then(
+          (value) => Future.delayed(const Duration(seconds: 1)).then(
+            (value) => _lottieAnimation.forward().then(
+                  (value) => Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const MyApp()),
+                  ),
+                ),
+          ),
+        );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: const Color(0xFF9ae79a),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedDefaultTextStyle(
+              duration: transitionDuration,
+              curve: Curves.fastOutSlowIn,
+              style: TextStyle(
+                color: const Color(0xFF4e954e),
+                fontSize: !expanded ? _bigFontSize : 35,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w600,
+              ),
+              child: const Text("D"),
+            ),
+            AnimatedCrossFade(
+              firstCurve: Curves.fastOutSlowIn,
+              crossFadeState: !expanded
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: transitionDuration,
+              firstChild: Container(),
+              secondChild: _logoRemainder(),
+              alignment: Alignment.centerLeft,
+              sizeCurve: Curves.easeInOut,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _logoRemainder() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          "ECARBONUS",
+          style: TextStyle(
+            color: Color(0xFF4e954e),
+            fontSize: 35,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        LottieBuilder.asset(
+          'assets/lottie/splash.json',
+          onLoaded: (composition) {
+            _lottieAnimation..duration = composition.duration;
+          },
+          frameRate: FrameRate.max,
+          repeat: false,
+          animate: false,
+          height: 90,
+          width: 90,
+          controller: _lottieAnimation,
+        )
+      ],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -29,40 +136,35 @@ class MyApp extends StatelessWidget {
           create: (_) => UserProvider(),
         ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Instagram Clone',
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              // Checking if the snapshot has any data or not
-              if (snapshot.hasData &&
-                  FirebaseAuth.instance.currentUser != null) {
-                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
-                return const ResponsiveLayout(
-                  mobileScreenLayout: GettingStarted(),
-                  webScreenLayout: WebScreenLayout(),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
-                );
-              }
-            }
-
-            // means connection to future hasnt been made yet
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.green,
-                ),
+      child: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            // Checking if the snapshot has any data or not
+            if (snapshot.hasData && FirebaseAuth.instance.currentUser != null) {
+              // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+              return const ResponsiveLayout(
+                mobileScreenLayout: GettingStarted(),
+                webScreenLayout: WebScreenLayout(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
               );
             }
+          }
 
-            return const OnboardingScreen();
-          },
-        ),
+          // means connection to future hasnt been made yet
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.green,
+              ),
+            );
+          }
+
+          return const OnboardingScreen();
+        },
       ),
     );
   }
