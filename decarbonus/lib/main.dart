9d4351658fac73f2209d466e815afa34a142ff92
Rohit Ranjan <ranjanrohit812@gmail.com>
2022-05-26 20:57:10 +0530

    Splash screen update

--- decarbonus/lib/main.dart
+++ decarbonus/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:decarbonus/providers/user_provider.dart';
 import 'package:decarbonus/responsive/responsive_layout.dart';
 import 'package:decarbonus/responsive/web_screen_layout.dart';
 import 'package:decarbonus/screens/getting_started.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -120,7 +121,7 @@ class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const MyApp();
+    return const SplashScreen();
   }
 }
 
@@ -158,6 +159,50 @@ class MyApp extends StatelessWidget {
 
         return const OnboardingScreen();
       },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: (5)),
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Lottie.asset(
+        'assets/lottie/decarbonus.json',
+        controller: _controller,
+        height: MediaQuery.of(context).size.height * 1,
+        animate: true,
+        onLoaded: (composition) {
+          _controller!
+            ..duration = composition.duration
+            ..forward().whenComplete(
+              () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyApp()),
+              ),
+            );
+        },
+      ),
     );
   }
 }
